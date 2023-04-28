9a084f36cc2fb2c1632f3f336fa81319cddcc72b
wee-owl <kem-cv@yandex.ru>
2023-04-28 16:03:07 +0300

    initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,21 @@
+import getLevel from '../app';
+import fetchData from '../http';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should call getLevel once', () => {
+  fetchData.mockReturnValue(JSON.stringify({}));
+
+  const response = getLevel(1);
+  expect(response)
+    .toEqual(
+      response.status === 'ok'
+        ? `Ваш текущий уровень: ${response.level}`
+        : 'Информация об уровне временно недоступна',
+    );
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
