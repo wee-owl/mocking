451d82f772a2b5f32bac31740a48fd5917bd404f
wee-owl <kem-cv@yandex.ru>
2023-05-16 15:15:55 +0300

    feat: update test file

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -7,15 +7,18 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
-test('check getLevel', () => {
-  fetchData.mockReturnValue(JSON.stringify({}));
-
-  const response = getLevel(1);
-  expect(response)
-    .toEqual(
-      response.status === 'ok'
-        ? `Ваш текущий уровень: ${response.level}`
-        : 'Информация об уровне временно недоступна',
-    );
+test('check getLevel(1)', () => {
+  const response = { status: 'ok', level: 1 };
+  fetchData.mockReturnValue(response);
+  const result = getLevel(1);
+  expect(result).toEqual('Ваш текущий уровень: 1');
   expect(fetchData).toBeCalledWith('https://server/user/1');
+});
+
+test('check getLevel(2)', () => {
+  const response = { status: null };
+  fetchData.mockReturnValue(response);
+  const result = getLevel(2);
+  expect(result).toEqual('Информация об уровне временно недоступна');
+  expect(fetchData).toBeCalledWith('https://server/user/2');
 });
